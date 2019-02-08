8baf64ccd3d777438c5111f4e4ab1da43c2363e7
lichet <46999953+lichet@users.noreply.github.com>
2019-02-08 15:49:21 +0900

    AI対応

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@
 //=========================================================
 // Botの準備
 //=========================================================
-
+/**
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
     process.exit(1);
@@ -25,7 +25,31 @@ var controller = Botkit.slackbot({
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
+*/
 
+const Botkit = require('botkit');
+const request = require('request');
+
+const controller = Botkit.slackbot();
+
+controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+controller.on('ambient', (bot, message) => {
+    request({
+        url: 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',
+        method: 'POST',
+        form: { apikey: process.env.a3rt_talk_apikey, query: message.text },
+        json:  true
+    }, (err, response, body) => {
+        if (body.status == 0) {
+            bot.reply(message, `${body.results[0].reply} (${Math.ceil(body.results[0].perplexity * 100) / 100})`);
+        } else {
+            bot.reply(message, `エラーたよ:fearful: [${body.status} ${body.message}]`);
+        }
+    });
+});
 
 
 //=========================================================
